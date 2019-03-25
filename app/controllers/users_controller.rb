a9e4407fee8dfe80ed1f7b2c0bf9d99f4848bfcf
github username <github email address>
2019-03-25 11:40:36 -0700

    added basic user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :password, :password_confirmation)
     end
   end
