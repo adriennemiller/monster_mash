5e5c1f56e41ca5b62aa7ff67e24003df453e34d7
bbpak <brianbpak@gmail.com>
2019-03-25 11:46:47 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,38 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create!([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create!(name: 'Luke', movie: movies.first)
+
+# USERS
+adrienne = User.create!(name: 'Adrienne')
+brian = User.create!(name: 'Brian')
+cody = User.create!(name: 'Cody')
+niky = User.create!(name: 'Niky')
+
+# MONSTERS
+sullivan = adrienne.monsters.create!(name: 'Sullivan')
+mike = adrienne.monsters.create!(name: 'Mike')
+vim = brian.monsters.create!(name: 'Vim')
+randal = brian.monsters.create!(name: 'Randal')
+boo = cody.monsters.create!(name: 'Boo')
+st = niky.monsters.create!(name: 'Storm Trooper')
+
+# BODY PARTS
+sullivan.create_leg
+sullivan.create_torso
+sullivan.create_head
+mike.create_leg
+mike.create_torso
+mike.create_head
+vim.create_leg
+vim.create_torso
+vim.create_head
+boo.create_leg
+boo.create_torso
+boo.create_head
+st.create_leg
+st.create_torso
+st.create_head
+
+
