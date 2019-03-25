bf2d2c9b38719c3df4213809c424b39ad097e11c
bbpak <brianbpak@gmail.com>
2019-03-25 11:17:36 -0700

    create basic views

--- app/controllers/legs_controller.rb
+++
@@ -1,2 +0,0 @@
-class LegsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  resources :legs
-  resources :torsos
-  resources :heads
   resources :monsters
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
