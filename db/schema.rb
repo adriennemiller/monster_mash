c287511da2b3d43df9d9930523023466cbddf367
github username <github email address>
2019-03-27 13:11:42 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_27_035453) do
+ActiveRecord::Schema.define(version: 2019_03_27_154002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "body_parts", force: :cascade do |t|
-    t.string "type"
+    t.string "section"
     t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
